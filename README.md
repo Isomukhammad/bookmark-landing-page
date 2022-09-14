40960637588e965e75ef9be5de38e7e81ab611a3
Isomukhammad <s.isomuxammad@gmail.com>
2022-09-14 22:11:39 +0500

    First commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Frontend Mentor - Bookmark landing page
+
+## Live Demo
+
+## Preview
+
+![Design preview for the Bookmark landing page coding challenge](./design/desktop-preview.jpg)
---
+++ script/script.js
@@ -0,0 +1,74 @@
+const FeaturesButtons = document.querySelectorAll('.category-button');
+const FeaturesImg = document.querySelector('#feature-image');
+const FeaturesTitle = document.querySelector('#features-title');
+const FeaturesDescription = document.querySelector('#features-description')
+const QuestionButtons = document.querySelectorAll('.question-button');
+const Question = document.querySelectorAll('.question');
+const Arrow = document.querySelectorAll('.arrow');
+const SubmitButton = document.querySelector('#submit-button');
+const Header = document.querySelector('header');
+const Hamburger = document.querySelector('#hamburger');
+const Logo = document.querySelector('#bookmark-logo');
+const Links = document.querySelector('#nav-links');
+
+let arr;
+
+fetch('data.json')
+    .then((res) => res.json())
+    .then(json => {arr = json})
+
+
+const removeAllActive = () => {
+    for(let i = 0; i < FeaturesButtons.length; i++){
+        FeaturesButtons[i].classList.value -= ' active'
+    }
+}
+
+for(let i = 0; i < FeaturesButtons?.length; i++){
+    FeaturesButtons[i].addEventListener('click', () => {
+        removeAllActive();
+        let {id, title, button, description, img} = arr[i];
+        FeaturesButtons[i].classList.value += ' active'
+        FeaturesImg.src = img;
+        FeaturesTitle.innerHTML = title;
+        FeaturesDescription.innerHTML = description;
+    })
+}
+
+const removeAllHeight = () => {
+    for(let i = 0; i < QuestionButtons?.length; i++){
+        Question[i].style.height = 0;
+    }
+}
+
+for(let i = 0; i < QuestionButtons?.length; i++){
+    QuestionButtons[i].addEventListener('click', () => {
+        if(Question[i].style.height == 'fit-content'){
+            Question[i].style.height = 0;
+            Arrow[i].style.transform = 'rotate(0deg)'
+        } else {
+            removeAllHeight();
+            Question[i].style.height = 'fit-content';
+            Arrow[i].style.transform = 'rotate(180deg)'
+        }
+    })
+    
+}
+
+SubmitButton.addEventListener('click', (e) => {
+    e.preventDefault();
+})
+
+Hamburger.addEventListener('click', () => {
+    if(Logo.src == 'http://127.0.0.1:5500/images/logo-bookmark.svg'){
+        Header.classList.value += 'header-active';
+        Logo.src = './images/logo-bookmark-footer.svg'  
+        Hamburger.src = './images/icon-close.svg'
+        Links.style.display = 'flex';
+    } else {
+        Header.classList.value = '';
+        Logo.src = './images/logo-bookmark.svg'
+        Hamburger.src = './images/icon-hamburger.svg'
+        Links.style.display = 'none';
+    }
+})
